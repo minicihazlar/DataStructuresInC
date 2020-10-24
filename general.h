6216eeced56bffee33f6ed0b4e20824e17001a68
Murat YILDIZ <47950996+minicihazlar@users.noreply.github.com>
2020-10-25 00:08:21 +0300

    Update general.h

--- general.h
+++ general.h
@@ -1,7 +1,18 @@
-#pragma once
+#ifndef __GENERAL_H
+#define __GENERAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // Find size of an arbitrary type
-#define num(x) (sizeof(x) / sizeof(*x))
+#define num(x)      (sizeof(x) / sizeof(*x))
 
 int general_gcd(int, int);
 double general_sqrt(int);
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __GENERAL_H
