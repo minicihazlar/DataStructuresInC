b72db6a9f4d33577cd16c65a8ba68a65ca89e737
Murat YILDIZ <47950996+minicihazlar@users.noreply.github.com>
2020-10-26 22:03:00 +0300

    Update my_llist.h

--- my_llist.h
+++ my_llist.h
@@ -9,18 +9,18 @@ extern "C" {
 typedef int DATA;
 
 // Forward declaration of the linked list data structure, implementation is hided
-typedef struct my_llist_s * my_llist_t; 
+typedef struct my_llist_s my_llist_t; 
 
 // Linked list handling functions
-void my_llist_ctor(my_llist_t *, DATA);
+my_llist_t * my_llist_ctor(DATA);
 void my_llist_dtor(my_llist_t *);
 int my_llist_get_size(my_llist_t *);
-int my_llist_push_head(my_llist_t *, DATA);
-int my_llist_push_end(my_llist_t *, DATA);
-int my_llist_pop_head(my_llist_t *, DATA *);
-int my_llist_pop_end(my_llist_t *, DATA *);
-int my_llist_pop_at(my_llist_t *, DATA *, int);
-void my_llist_reverse(my_llist_t *);
+int my_llist_push_head(my_llist_t **, DATA);
+int my_llist_push_end(my_llist_t **, DATA);
+int my_llist_pop_head(my_llist_t **, DATA *);
+int my_llist_pop_end(my_llist_t **, DATA *);
+int my_llist_pop_at(my_llist_t **, DATA *, int);
+void my_llist_reverse(my_llist_t **);
 
 #ifdef __cplusplus
 }
